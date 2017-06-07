2bdb658fe48bbe48e51fc58c329e68a051631b36
fsboc <zhu.jincao@gmail.com>
2017-06-07 13:00:17 -0600

    set sensor_delay_fastest

--- gyromorse/app/src/main/java/com/jc/gyromorse/MainActivity.java
+++ gyromorse/app/src/main/java/com/jc/gyromorse/MainActivity.java
@@ -59,7 +59,7 @@ public class MainActivity extends Activity implements SensorEventListener{
         // add listener. The listener will be MyActivity (this) class
         sensorManager.registerListener(this,
                 sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
-                SensorManager.SENSOR_DELAY_NORMAL);
+                SensorManager.SENSOR_DELAY_FASTEST);
 
         //copy from  //adding accelerometer data list values for the starting
         this.accelerometerDataList = new ArrayList<AccelerometerClass>();
